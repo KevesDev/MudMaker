30bafa9e44e6bc18a8beccfe7aa321a1507d563b
Riane <42573189+KevesDev@users.noreply.github.com>
2026-04-13 17:28:06 -0600

    initial

---
+++ MudMaker/src/main/java/com/keves/mudmaker/MudMakerApplication.java
@@ -0,0 +1,13 @@
+package com.keves.mudmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MudMakerApplication.class, args);
+    }
+
+}
---
+++ MudMaker/src/test/java/com/keves/mudmaker/MudMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keves.mudmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
